0e6ad76941dc321aa76ef5c30fcdf1cd934d322c
Swiggui <figueredojuan2020@outlook.es>
2022-11-14 22:41:20 -0500

    Cambios en el backend

--- src/controllers/controllerDulce.js
+++ src/controllers/controllerDulce.js
@@ -1,11 +1,37 @@
 const Dulce = require("../models/Dulces");
+const jwt = require("jsonwebtoken");
 
 //Guardar dulce
 const guardarDulces = async (req, res) => {
     try {
-        const dulce = new Dulce(req.body);
-        await dulce.save();
-        res.send("Dulce almacenado correctamente");
+        const { referencia } = req.body;
+        let candie = await Dulce.findOne({referencia});
+
+        if(candie){
+            return res.status(400).json({ mensaje: "Dulce Existente" });
+        } else {
+            const dulce = new Dulce(req.body);
+            await dulce.save();
+            res.status(200).json({aviso: "Dulce almacenado correctamente"});
+        }
+
+        const payload = {
+            dulce: { id: dulce.id },
+          };
+      
+        jwt.sign(
+            payload,
+            process.env.SECRETA,
+            {
+              expiresIn: 3600, //1 hora
+            },
+            (error, token) => {
+              if (error) throw error;
+      
+              //Mensaje de confirmación
+              res.json({ token });
+            }
+        );
     } catch (error) {
         console.log(error);
     }
@@ -25,8 +51,12 @@ const listarDulces = async (req, res) => {
 const dulcesporID = async (req, res) => {
     try {
         const id = req.params.id;
-        const dulce = await Dulce.findById(id);
-        res.status(200).send(dulce);
+        if(id){
+            const dulce = await Dulce.findById(id);
+            res.status(200).send(dulce);
+        } else {
+            res.send("No se puede tramitar la petición"); 
+        }
     } catch (error) {
         console.log(error);
     }
@@ -37,7 +67,23 @@ const eliminarDulce = async (req, res) => {
     try {
         const id = req.params.id;
         await Dulce.findByIdAndDelete(id);
-        res.send("Dulce eliminado correctamente");
+        res.status(200).json({ aviso: "Dulce eliminado correctamente"});
+
+        const payload = {
+            dulce: { id: dulce.id },
+        };
+        jwt.sign(
+            payload,
+            process.env.SECRETA,
+            {
+              expiresIn: 3600, //1 hora
+            },
+            (error, token) => {
+              if (error) throw error;
+              //Mensaje de confirmación
+              res.json({ token });
+            }
+        );
     } catch (error) {
         console.log(error);
     }
@@ -49,7 +95,24 @@ const editarDulces = async (req, res) => {
         const id = req.params.id;
         const dulce = req.body;
         await Dulce.findByIdAndUpdate(id, dulce);
-        res.send("Dulce editado correctamente");
+        return res.status(200).json({ aviso: "Dulce editado correctamente"});
+        const payload = {
+            dulce: { id: dulce.id },
+        };
+      
+        jwt.sign(
+            payload,
+            process.env.SECRETA,
+            {
+              expiresIn: 3600, //1 hora
+            },
+            (error, token) => {
+              if (error) throw error;
+      
+              //Mensaje de confirmación
+              res.json({ token });
+            }
+        );
     } catch (error) {
         console.log(error);
     }
